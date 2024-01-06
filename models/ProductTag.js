848975a36fe3eff6bc902a8e5add722edc5971f4
jalland <jjalland@gmail.com>
2024-01-05 19:22:37 -0600

    Added models and initial routes for the models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // TODO: reader comes from the "Reader.js" model
+        model: 'product',
+        key: 'id',
+      },
+    }, 
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // TODO: reader comes from the "Reader.js" model
+        model: 'tag',
+        key: 'id',
+      },
+    }, 
   },
   {
     sequelize,
